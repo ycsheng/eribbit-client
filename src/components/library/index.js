51f6f6422c3ca94cf83a7431497eacb21b802ca0
changsheng <2578248487@qq.com>
2021-10-09 13:24:31 +0800

    轮播图

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -13,11 +13,13 @@
  */ 
 
 import XtxSkeleton from './xtx-skeleton.vue'
+import XtxCarousel from './xtx-carousel.vue'
 
 export default {
   install(app) {
     // 在app上进行扩展，app提供 compont directive 函数
     // 如果要挂载原型 app.config.globalProperties 方式
     app.component(XtxSkeleton.name, XtxSkeleton)
+    app.component(XtxCarousel.name, XtxCarousel)
   }
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,24 +1,29 @@
 <template>
-  <div class="container">
-    <!-- 首页入口（分类+广告） -->
-    <div class="home-entry">
+  <!-- 首页入口（分类+广告）-->
+  <div class="home-entry">
+    <div class="container">
       <!-- 左侧分类 -->
       <HomeCategory />
       <!-- 轮播图 -->
+      <HomeBanner />
     </div>
     <!-- 测试骨架效果 -->
     <!-- <XtxSkeleton width="200px" height="200px" bg="red" animated/> -->
   </div>
+  <!-- 新鲜好物 -->
+  <!-- 人气推荐 -->
 </template>
 
 <script>
 import HomeCategory from './components/home-category.vue'
+import HomeBanner from './components/home-banner.vue'
 // 每使用一次就要导入，采用Vue插件的形式
 // import XtxSkeleton from '@/components/library/xtx-skeleton.vue'
 export default {
   name: 'HomePage',
   components: {
     HomeCategory,
+    HomeBanner,
   }
 }
 </script>
